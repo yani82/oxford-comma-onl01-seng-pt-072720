cea45e5de362bcf03f894636094d09e40a95c1ec
Yani Asari <yani.iman1@gmail.com>
2020-08-04 06:08:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,19 @@
 def oxford_comma(array)
-["kiwi"].join
-end
-
+array = [*array]
+case array.size
+when 0 
+  ''
+  when 1 
+    array[0].to_s 
+    when 2
+      array.join(' and ')
+    else 
+      array_copy = array 
+      array_copy [-1] = "and #{array_copy[-1]}"
+      array_copy.join(',')
+    end 
+  end 
+      
 
 
 
