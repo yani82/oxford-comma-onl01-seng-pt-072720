4a925e4bd08fc83b0cc2aed3f608d31e1e199a74
Yani Asari <yani.iman1@gmail.com>
2020-08-04 06:18:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+case array.size
+when 0 
+  ''
+  when 1 
+    array.first
+    when 2
+      array.join(' and ')
+    else 
+      [array[0..-2].join(', '), array.last].join(', and ')
+    end 
+  end 
+      
 
-end
+
+
+
